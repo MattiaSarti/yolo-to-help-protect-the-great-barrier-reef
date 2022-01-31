51ab7706ed80baeb1ba23d90c6dbebfc4a446aff
MattiaSarti <nmattia1@gmail.com>
2022-01-31 09:51:34 +0100

    🦀 ADD - notes on utilities 🐬

--- source/loss_and_metrics.py
+++ source/loss_and_metrics.py
@@ -3,4 +3,6 @@ Definitions of the employed loss function and metrics.
 """
 
 
+# TODO: tf.image.non_max_suppression,  tf.image.non_max_suppression_overlaps, tf.image.non_max_suppression_padded, tf.image.non_max_suppression_with_scores
+
 raise NotImplementedError
