7c9f2efae847912cf65e8b6f034de4e1102abcf4
MattiaSarti <nmattia1@gmail.com>
2022-02-05 10:36:43 +0100

    🦀 UPDATE - training process with new sets 🐬

--- source/training_and_validation.py
+++ source/training_and_validation.py
@@ -15,14 +15,21 @@ from loss_and_metrics import (
     iou_threshold_averaged_f2_score, yolov3_variant_loss
 )
 from model_architecture import YOLOv3Variant
-from samples_and_labels import dataset_of_samples_and_model_outputs
+from samples_and_labels import (
+    dataset_of_samples_and_model_outputs,
+    split_dataset_into_batched_training_and_validation_sets
+)
 
 
 LEARNING_RATE = 1e-3
 N_EPOCHS = 10
 
 
-def train_model(model_instance: Model, dataset: Dataset) -> str:  # TODO: output dtype
+def train_model(
+        model_instance: Model,
+        training_set: Dataset,
+        validation_set: Dataset
+) -> str:  # TODO: output dtype
     """
     Compile (in TensorFlow's language acception, i.e. associate optimizer,
     loss function and metrics to the model instance) the input model instance
@@ -35,18 +42,24 @@ def train_model(model_instance: Model, dataset: Dataset) -> str:  # TODO: output
     )
 
     training_history = model_instance.fit(
-        x=dataset,
-        epochs=N_EPOCHS
+        x=training_set,
+        epochs=N_EPOCHS,
+        validation_data=validation_set
     )
 
     return training_history
 
 
 if __name__ == '__main__':
-    samples_and_labels = dataset_of_samples_and_model_outputs()
+    (
+        training_samples_and_labels, validation_samples_and_labels
+    ) = split_dataset_into_batched_training_and_validation_sets(
+        training_plus_validation_set=dataset_of_samples_and_model_outputs()
+    )
     model = YOLOv3Variant()
 
     training_history = train_model(
         model_instance=model,
-        dataset=samples_and_labels
+        training_set=training_samples_and_labels,
+        validation_set=validation_samples_and_labels,
     )
