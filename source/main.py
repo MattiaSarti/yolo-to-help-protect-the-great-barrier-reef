8d5bc47980aed8123c429f5f15a3facbe530010d
MattiaSarti <nmattia1@gmail.com>
2022-02-07 12:55:51 +0100

    🦀 ADD - main content 🐬

--- source/main.py
+++ source/main.py
@@ -10,6 +10,14 @@ from numpy.random import seed as numpy_seed
 from tensorflow.random import set_seed
 # pylint: enable=import-error
 
+if __name__ != 'main_by_mattia':
+    from model_architecture import YOLOv3Variant
+    from samples_and_labels import (
+        dataset_of_samples_and_model_outputs,
+        split_dataset_into_batched_training_and_validation_sets
+    )
+    from training_and_validation import train_and_validate_model
+
 
 def fix_seeds_for_reproducible_results() -> None:
     """
@@ -27,4 +35,20 @@ def main() -> None:
     """
     fix_seeds_for_reproducible_results()
 
-    raise NotImplementedError
+    (
+        training_samples_and_labels, validation_samples_and_labels
+    ) = split_dataset_into_batched_training_and_validation_sets(
+        training_plus_validation_set=dataset_of_samples_and_model_outputs()
+    )
+
+    model = YOLOv3Variant()
+
+    training_history = train_and_validate_model(
+        model_instance=model,
+        training_set=training_samples_and_labels,
+        validation_set=validation_samples_and_labels,
+    )
+
+
+if __name__ == 'main_by_mattia':
+    main()
