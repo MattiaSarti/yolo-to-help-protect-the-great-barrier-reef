3afae16e7f3001b0350f7d10abd76c165b73398f
MattiaSarti <nmattia1@gmail.com>
2022-02-10 18:55:48 +0100

    🦀 UPDATE - hyperparameters  🐬

--- source/samples_and_labels.py
+++ source/samples_and_labels.py
@@ -53,7 +53,7 @@ if __name__ != 'main_by_mattia':
     )
 
 
-MINI_BATCH_SIZE = 2  # 4  # TODO
+MINI_BATCH_SIZE = 16  # TODO
 VALIDATION_SET_PORTION_OF_DATA = 0.1  # 0.3
 
 # only when running everything in a unified notebook on Kaggle's servers:
